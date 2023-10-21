3464b49bb233f97988d36bb33be497649affa8ff
Melody Abi-Khalil <melody.abikhalil@hungerstation.com>
2023-10-21 18:54:20 +0400

    test: add new migration with sql syntax error

---
+++ db/migrations/20231021145327_offer-rules-table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE offer_rules
+(
+    id         bigserial primary key,
+    offer_id   bigint,
+    rule_type  varchar(50) not null,
+    rule_data  jsonb default '{}'::jsonb,
+
+    legacy_id  varchar(50),
+
+    created_at timestamp   not null
+    updated_at timestamp   not null,
+    deleted_at timestamp
+);
+
+
